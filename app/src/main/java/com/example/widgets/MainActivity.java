bde09ebc301662ec0538c172f893b10ab5d51936
i19matla <i19matla@student.his.se>
2021-04-11 20:41:04 +0200

    Lade till en kommentar för lärandet om knappbibliotek.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.Button; // <-- Denna lades till automatiskt då jag tryckte enter vid ett tillfälle då id:t skrevs in.
+                                //Utan den kommer inte knappen att senare fungera, importerar ett knappbibliotek.
 
 public class MainActivity extends AppCompatActivity {
 
