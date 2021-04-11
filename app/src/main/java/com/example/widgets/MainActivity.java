efb57fd4cd2d381170ebcfe7de544a5123af3190
i19matla <i19matla@student.his.se>
2021-04-11 20:56:24 +0200

    Lade in ett sista kodblock till knappen som skriver ut till loggen att knappen är klickad.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button; // <-- Denna lades till automatiskt då jag tryckte enter vid ett tillfälle då id:t skrevs in.
                                 //Utan den kommer inte knappen att senare fungera, importerar ett knappbibliotek.
 
@@ -14,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button b = findViewById(R.id.knappensid);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","Knappen är klickad! Tjohoooooooo!");
+            }
+        });
     }
-}
+    }
+
